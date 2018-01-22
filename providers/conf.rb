c292a8abdd99c5e80e634e204954bc606c8bb0a8
Chris <chris@emptyset.io>
2018-01-22 16:55:29 -0700

    give proper path to plugin in provider

--- providers/conf.rb
+++ providers/conf.rb
@@ -29,7 +29,7 @@ action :create do
       config: new_resource.config || node['openvpn']['config'],
       push_routes: node['openvpn']['push_routes'],
       push_options: node['openvpn']['push_options'],
-      plugins: node['openvpn']['plugin']
+      plugins: node['openvpn']['config']['plugin']
     )
     helpers do
       # rubocop:disable Metrics/MethodLength
