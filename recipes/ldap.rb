b7f122f87e3aa3475bfbd484f758a951eeb7a06a
Chris <chris@emptyset.io>
2018-01-22 17:20:47 -0700

    lets use the package instead of building from src -_-

--- recipes/ldap.rb
+++ recipes/ldap.rb
@@ -4,7 +4,7 @@
 #
 # Author:: Luke and Nathan
 #
-node.default['openvpn']['config']['plugin'] << "/usr/local/lib/openvpn-auth-ldap.so #{node['openvpn']['ldap']['config']['auth_dir']}/auth-ldap.conf"
+node.default['openvpn']['config']['plugin'] << "/usr/lib/openvpn/openvpn-auth-ldap.so #{node['openvpn']['ldap']['config']['auth_dir']}/auth-ldap.conf"
 node.default['openvpn']['config']['client-cert-not-required'] = ''
 
 ldap_url = node['openvpn']['ldap']['source']['url']
@@ -19,40 +19,7 @@ remote_file ldap_url do
 end
 
 
-# source install depends on the existence of the `tar` package
-package 'tar'
-
-# required to build openvpn-auth-ldap
-package 're2c'
-package 'gcc-4.6'
-package 'g++-4.6'
-package 'gobjc-4.6'
-
-
-# openldap headers
-package 'libldap2-dev'
-
-# Unpack downloaded source so we could apply nginx patches
-# in custom modules - example http://yaoweibin.github.io/nginx_tcp_proxy_module/
-# patch -p1 < /path/to/nginx_tcp_proxy_module/tcp.patch
-bash 'unarchive_source' do
-  code <<-EOH
-    tar zxf #{Chef::Config['file_cache_path']}/#{::File.basename(ldap_src_filepath)}.tar.gz -C #{::File.dirname(ldap_src_filepath)}
-  EOH
-  not_if { ::File.directory?(ldap_src_filepath) }
-end
-
-bash 'compile_and_configure_ldap' do
-  cwd ldap_src_filepath
-  code <<-EOS
-    ./regen.sh && \
-    ./configure --prefix=/usr/local --with-openvpn=#{node['openvpn']['source']['filepath']}/include --with-objc-runtime=GNU CC="gcc-4.6 -fPIC" OBJC=gcc-4.6 OBJCPP=cpp-4.6 && \
-    make && \
-    sudo make install
-  EOS
-
-  not_if { ::File.file?('/opt/ldap/ldap_openvpn.so') }
-end
+package 'openvpn-auth-ldap'
 
 directory node['openvpn']['ldap']['config']['auth_dir'] do
   action :create
