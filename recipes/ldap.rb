72fab51b5342eb20ad5e00844b2103d64b39942f
Chris <chris@emptyset.io>
2018-01-22 18:35:53 -0700

    use override

--- recipes/ldap.rb
+++ recipes/ldap.rb
@@ -4,9 +4,8 @@
 #
 # Author:: Luke and Nathan
 #
-node.default['openvpn']['config']['plugin'] = [] unless node.default['openvpn']['config']['plugin'].is_a? Array
-node.default['openvpn']['config']['plugin'] << "/usr/lib/openvpn/openvpn-auth-ldap.so #{node['openvpn']['ldap']['config']['auth_dir']}/auth-ldap.conf"
-node.default['openvpn']['config']['client-cert-not-required'] = ''
+node.override['openvpn']['config']['plugin'] << "/usr/lib/openvpn/openvpn-auth-ldap.so #{node['openvpn']['ldap']['config']['auth_dir']}/auth-ldap.conf"
+node.override['openvpn']['config']['client-cert-not-required'] = ''
 
 ldap_url = node['openvpn']['ldap']['source']['url']
 
